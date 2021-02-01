02f3654432c5c6036e34cb501f01217bbc46b4d1
lafertsd <77639125+lafertsd@users.noreply.github.com>
2021-02-01 19:13:40 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,18 +5,22 @@
 // burger.addEventListener('click', function(){
 //     nav.classList.toggle('nav-list-activ');
 // })
+
 const windowWidth = window.innerWidth;
 $('.menu').click(function(){
     $('.nav-list').toggleClass('nav-list-activ');
     $('.content').toggleClass('content-activ');
+    body.classList.toggle('active');
     if (windowWidth <= 480) {
         $('.content').removeClass('content-activ');
+        $('body').classList.toggle('active');
     }
 
 })
 $('.btn-remove').click(function(){
     $('.nav-list').removeClass('nav-list-activ');
     $('.content').removeClass('content-activ');
+    $('body').removeClass('active');
 })
 $('.nav-links li').click(function() {
     $('.nav-list').removeClass('nav-list-activ');
